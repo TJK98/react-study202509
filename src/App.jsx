c0eeb813ad72a8fa00f46f07a24a8bcff26c24de
TJK98 <wtj1998@naver.com>
2025-08-27 19:52:02 +0900

    1-4-2. props 기초 - ExpenseItem 컴포넌트가 props를 사용하도록 수정: title, price, date 전달 및 출력 - App 컴포넌트에서 다양한 지출항목 데이터 전달 및 렌더링 추가 - 기존 하드코딩된 값을 props 기반으로 동적 처리

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import ExpenseItem from "./components/expenses/ExpenseItem.jsx";
 
 const App = () => (
     <>
-        <ExpenseItem />
+        <ExpenseItem
+            title='피자'
+            price={20000}
+            date={new Date(2025, 7, 13)}
+        /> {/*문자열만 {} 생략 가능, 마치 함수에게 파라미터를 전달하는 효과*/}
+        <ExpenseItem
+            title='치킨'
+            price={18000}
+            date={new Date(2025, 8, 7)}
+        />
+        <ExpenseItem
+            title='간짜장'
+            price={9000}
+            date={new Date(2025, 1, 19)}
+        />
     </>
 );
 
--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './ExpenseItem.css'; // 전용 css를 넣을 때 import 해주는 게 리액트의 방식이다.
 
-const ExpenseItem = () => {
+const ExpenseItem = ({title, price, date})/*(props)*//*title이라고 하지 않고 props로 전달*/ => {
+
+    // console.log(props); // props의 정체는 객체
+    // console.log(props.title); // title의 값을 꺼내려면 props가 아닌 title을 꺼내야한다.
+
+    // const {title, price, date} = props;
+
     return (
         <div className='expense-item'>
-            <div>2025-08-024</div>
+            {/*<div>2025-08-24</div>*/}
+            <div>{date.toLocaleString()}</div>
             <div className='expense-item__description'>
-                <h2>점심밥</h2>
-                <div className='expense-item__price'>10000원</div>
+                {/*<h2>점심밥</h2>*/}
+                <h2>{title}</h2>
+                {/*<div className='expense-item__price'>10000원</div>*/}
+                <div className='expense-item__price'>{price}원</div>
             </div>
         </div>
     );
