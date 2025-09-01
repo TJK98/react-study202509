0e6618e0b9f944b9961978a2a57fe742ce22005a
TJK98 <wtj1998@naver.com>
2025-09-01 15:32:59 +0900

    2-3-2. 입력값 단일 상태 관리 - useState를 활용하여 title, price, date 상태 관리 로직 추가 - form 제출 시 상태값을 기반으로 payload 생성 및 console 출력 - 입력 필드에 onInput 이벤트 핸들러 추가

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+    // 입력값을 단일 값 상태관리
+    const [title, setTitle] = useState(``);
+    const [price, setPrice] = useState();
+    const [date, setDate] = useState(null);
+
     // 오늘 날짜를 YYYY-MM-DD 형식으로 가져오는 함수
     const getTodayDate = () => {
         const today = new Date();
@@ -11,12 +17,36 @@ const ExpenseForm = () => {
         return `${year}-${month}-${day}`;
     };
 
+    // form 제출 이벤트
+    const handleSubmit = e => {
+        // submit의 기본 제출은 새로고침, e.preventDefault()로 새로고침 막아주기
+        e.preventDefault()
+        // console.log(`form 제출`);
+
+        // const $inputElements = document.querySelectorAll(`input`);
+
+        // 실시간이 아닌 입력된 값을 가져오는 것
+        // const payload = {
+        //     // title: $inputElements[0].value,
+        //     // price: $inputElements[1].value,
+        //     // date: $inputElements[2].value
+        //
+        //     title: title,
+        //     price: price,
+        //     date: date
+        // };
+
+        const payload = { title, price, date };
+
+        console.log(payload);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onInput={e => setTitle(e.target.value)} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Price</label>
@@ -24,6 +54,7 @@ const ExpenseForm = () => {
                         type='number'
                         min='100'
                         step='100'
+                        onInput={e => setPrice(+e.target.value)}
                     />
                 </div>
                 <div className='new-expense__control'>
@@ -32,6 +63,7 @@ const ExpenseForm = () => {
                         type='date'
                         min='2019-01-01'
                         max={getTodayDate()}
+                        onInput={e => setDate(+e.target.value)}
                     />
                 </div>
             </div>
