1313babff210f92ce3b44efcf9c16db9ce6e38e9
TJK98 <wtj1998@naver.com>
2025-09-04 11:38:48 +0900

    4-1-1. 동적 인라인 스타일 조작 - 입력값 유효성 검사 로직 추가 (빈 값 입력 시 오류 처리) - 입력 상태(`isValid`)에 따라 레이블 및 입력 필드 스타일 동적 변경 - goalInputHandler 함수로 입력 처리 로직 분리 및 상태 업데이트 - 주석 추가: 상태 초기값과 null 처리 이유 상세 설명

--- src/components/CourseGoals/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput.jsx
@@ -7,25 +7,54 @@ const CourseInput = ({onAdd}) => {
     // 목표 input에 입력한 상태 관리
     const [enteredText, setEnteredText] = useState(``);
 
+    // 입력값에 오류가 있는지 여부를 상태 관리
+    // 초기값을 false가 아닌 null로 주는 이유는 사용자가 아무런 입력을 하지 않았어도 false가 뜰 수 있기 때문에 null로 줘야 한다.
+    const [isValid, setIsValid] = useState(null);
+
     const submitHandler = e => {
         // onSubmit인 submit 이벤트의 새로고침 막기
         e.preventDefault();
-        console.log(`입력값: ${enteredText}`);
+
+        // 입력값 검증
+        if (!enteredText.trim()) {
+            setIsValid(false);
+            return;
+        }
+
+        // console.log(`입력값: ${enteredText}`);
 
         onAdd(enteredText);
 
         // 입력값 비우기
         setEnteredText(``);
+
+        setIsValid(true);
+    };
+
+    const goalInputHandler = e => {
+        const inputValue = e.target.value;
+
+        // 입력값 검증
+        if (inputValue.trim()) {
+            setIsValid(true);
+        }
+
+        return setEnteredText(inputValue);
     };
 
     return (
         <form onSubmit={submitHandler}>
             <div className="form-control">
-                <label>나의 목표</label>
+                {/*null도 false 취급이 되기 때문에 false 일 때만을 명시해줘야 된다.*/}
+                <label style={{color: isValid !== false ? "black" : "red"}}>나의 목표</label>
                 <input
                     type="text"
-                    onInput={e => setEnteredText(e.target.value)}
+                    onInput={goalInputHandler}
                     value={enteredText} // 양방향 매핑
+                    style={{
+                        background: isValid !== false ? 'transparent' : 'salmon', // null도 false 취급이 되기 때문에 false 일 때만을 명시해줘야 된다.
+                        borderColor: isValid !== false ? 'black' : 'red' // null도 false 취급이 되기 때문에 false 일 때만을 명시해줘야 된다.
+                    }}
                 />
             </div>
             <Button type="submit">목표 추가하기</Button>
