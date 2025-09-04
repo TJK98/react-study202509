b33a8cb4bd5ee609a65109e3d134bbb22de65fa0
TJK98 <wtj1998@naver.com>
2025-09-04 19:26:36 +0900

    5-2-1. 사용자 가입 템플릿 - AddUsers 및 UserList 컴포넌트 생성 - App 컴포넌트에서 상태(userList) 기반 회원 정보 관리 구현 - CSS 모듈 기반 스타일링 적용 (AddUsers.module.css, UserList.module.css)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import TodoTemplate from './components/Todos/TodoTemplate.jsx';
+import AddUsers from "./components/Users/AddUsers.jsx";
+import UserList from "./components/Users/UserList.jsx";
 
 // 리액트의 props의 한계점 : 단방향이다. (위에서 아래로) 그래서 서버에서 날라오는 데이터들은 위에서 처리하는 게 좋다.
 const App = () => {
 
-    return <TodoTemplate />
+    // 회원들이 저장될 배열
+    const [userList, setUserList] = useState([]);
+
+    // 입력한 회원정보를 가져오는 함수
+    const addUserInfo = (user) => {
+        setUserList([...userList, user]);
+    };
+
+    return (
+        <>
+            <AddUsers onAddUser={addUserInfo} />
+            <UserList users={userList} />
+        </>
+    )
 };
 
 export default App;
