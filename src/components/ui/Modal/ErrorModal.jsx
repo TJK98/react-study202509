46132faab5da5c394570bf9b30e5d61be195e00a
TJK98 <wtj1998@naver.com>
2025-09-04 20:12:18 +0900

    5-2-5. React Portal 적용 - ErrorModal 컴포넌트를 Backdrop과 ModalOverlay로 분리하여 재사용성 향상 - ReactDOM.createPortal을 활용하여 ErrorModal을 root 외부 DOM으로 렌더링 - index.html에 backdrop-root 및 modal-overlay-root 추가

--- src/components/ui/Modal/ErrorModal.jsx
+++ src/components/ui/Modal/ErrorModal.jsx
@@ -3,21 +3,60 @@ import Card from '../Card';
 import Button from '../Button';
 import styles from './ErrorModal.module.css';
 
+// portal을 사용하기 위한 import
+import ReactDOM from 'react-dom';
+
+// 컴포넌트 안에 컴포넌트 여러 개 생성 가능
+const Backdrop = ({onClose}) => {
+    return <div className={styles.backdrop} onClick={onClose}></div>
+}
+
+const ModalOverlay = ({title, message, onClose}) => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={onClose}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
+
 const ErrorModal = ({ title, message, onClose }) => {
     return (
         <>
-            <div className={styles.backdrop} onClick={onClose}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={onClose}>Okay</Button>
-                </footer>
-            </Card>
+            {
+                // <div className={styles.backdrop} onClick={onClose}></div>를 portal에 태워서 index.html의 document.getElementById(`backdrop-root`)로 보낸다.
+                ReactDOM.createPortal(
+                    <Backdrop onClose={onClose} />,
+                    document.getElementById(`backdrop-root`)
+                )
+            }
+
+            {
+                ReactDOM.createPortal(
+                    <ModalOverlay title={title} message={message} onClose={onClose} />,
+                    document.getElementById(`modal-overlay-root`)
+                )
+            }
+
+            {/*<div className={styles.backdrop} onClick={onClose}></div>*/}
+            {/*<Card className={styles.modal}>*/}
+            {/*    <header className={styles.header}>*/}
+            {/*        <h2>{title}</h2>*/}
+            {/*    </header>*/}
+            {/*    <div className={styles.content}>*/}
+            {/*        <p>{message}</p>*/}
+            {/*    </div>*/}
+            {/*    <footer className={styles.actions}>*/}
+            {/*        <Button onClick={onClose}>Okay</Button>*/}
+            {/*    </footer>*/}
+            {/*</Card>*/}
         </>
     );
 };
