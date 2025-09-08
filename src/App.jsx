c1b5f74ed18cc39c5e9b66b58a14244123c3fb7c
TJK98 <wtj1998@naver.com>
2025-09-08 19:47:26 +0900

    6-1-1. side effect 템플릿 - MainHeader, Navigation, Home, Login 컴포넌트 생성 및 스타일링(CSS 모듈 활용) - App 컴포넌트 구조 변경: 메인 헤더와 로그인 화면 렌더링 추가 - 메인 레이아웃 및 UI 개선을 위한 CSS, 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,22 @@ import React, {useState} from 'react';
 import './App.css';
 import AddUsers from "./components/Users/AddUsers.jsx";
 import UserList from "./components/Users/UserList.jsx";
+import MainHeader from "./components/SideEffect/MainHeader.jsx";
+import Home from "./components/SideEffect/Home.jsx";
+import Login from "./components/SideEffect/Login.jsx";
 
 // 리액트의 props의 한계점 : 단방향이다. (위에서 아래로) 그래서 서버에서 날라오는 데이터들은 위에서 처리하는 게 좋다.
 const App = () => {
 
-    // 회원들이 저장될 배열
-    const [userList, setUserList] = useState([]);
-
-    // 입력한 회원정보를 가져오는 함수
-    const addUserInfo = (user) => {
-        setUserList([...userList, user]);
-    };
-
     return (
         <>
-            <AddUsers onAddUser={addUserInfo} />
-            <UserList users={userList} />
+            <MainHeader/>
+            <main>
+                <Home />
+                <Login />
+            </main>
         </>
-    )
+    );
 };
 
 export default App;
