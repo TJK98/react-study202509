d42f00aab5fb87873ae786db2384d74fa9c22aea
TJK98 <wtj1998@naver.com>
2025-09-08 20:26:44 +0900

    6-1-2, 3. 로그인 기능 구현 및 useEffect로 side effect처리 - App 컴포넌트에 로그인 상태 관리 로직(useState, useEffect) 추가 - MainHeader 및 Navigation 컴포넌트에 logout 핸들러 전달 및 버튼 구현 - Login 컴포넌트에서 입력값 검증 및 로그인 처리 로직 추가 - Button 컴포넌트에 disabled prop 추가, 폼 유효성에 따른 활성화 제어 - 로컬 스토리지를 활용한 로그인 상태 유지 및 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import AddUsers from "./components/Users/AddUsers.jsx";
 import UserList from "./components/Users/UserList.jsx";
@@ -9,12 +9,51 @@ import Login from "./components/SideEffect/Login.jsx";
 // 리액트의 props의 한계점 : 단방향이다. (위에서 아래로) 그래서 서버에서 날라오는 데이터들은 위에서 처리하는 게 좋다.
 const App = () => {
 
+    // 로그인 상태 체크
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // 토큰 검사
+    // console.log(`토큰 검사`);
+    // const token = localStorage.getItem(`token`)
+    // if (token === `1`) {
+    //     setIsLoggedIn(true); // 리렌더링 발생
+    // }
+
+    // side effect (서버에서 데이트를 가져오거나 외부 비동기 작업) 처리를 위한 훅(리액트 함수)
+    useEffect(() => {
+        console.log(`useEffect 실행`);
+
+        // 리랜더링 되어 발생하는 무한 로딩에 대한 로직을 useEffect에 넣으면 된다.
+        const token = localStorage.getItem(`token`)
+        if (token === `1`) {
+            setIsLoggedIn(true); // 리렌더링 발생
+        }
+    },[]);
+
+    const handleLogin = (email, password) => {
+
+        // 로그인 검증
+        if (email === `abc@def.com` && password === `12345678`) {
+            localStorage.setItem(`token`, `1`);
+            setIsLoggedIn(true);
+        } else {
+            alert(`로그인 실패`);
+        }
+    };
+
+    // console.log(`랜더링 수행`);
+
+    const handleLogout = () => {
+      localStorage.removeItem(`token`);
+      setIsLoggedIn(false);
+    };
+
     return (
         <>
-            <MainHeader/>
+            <MainHeader onLogout={handleLogout}/>
             <main>
-                <Home />
-                <Login />
+                {isLoggedIn && <Home/>}
+                {!isLoggedIn && <Login onLogin={handleLogin}/>}
             </main>
         </>
     );
